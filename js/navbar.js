7d0fff5a3ed7f51d7173ac096126bc348ac0d8bd
PacThePenguinBoi <alechase2008@gmail.com>
2026-01-08 21:58:30 -0700

    Update navbar.js
    Removed unecessary code

--- js/navbar.js
+++ js/navbar.js
@@ -2,10 +2,7 @@ let menuOpened = false;
 
 function closeMenu() {
     menuOpened=false;
-    document.getElementById("menucloser").style.opacity = "0";
-    document.getElementById("menucloser").style.transform = "rotate(90deg)";
     document.getElementById('menu-toggle').checked = false;
-    document.getElementById("linesButton").style.opacity = "1";
     document.getElementById("linesButton").style.transform = "rotate(0deg)";
     document.getElementById("linesButton").style.transition = "all 0.5s ease-in-out";
     document.getElementById("main-page-contents").style = "-webkit-filter: blur(0px); filter: blur(0px); margin-top: 40px;";
@@ -16,9 +13,6 @@ function openMenu() {
     if (!menuOpened) {
         menuOpened = true;
         document.getElementById('menu-toggle').checked = false;
-        document.getElementById("menucloser").style.opacity = "1";
-        document.getElementById("menucloser").style.transform = "rotate(0deg)";
-        document.getElementById("linesButton").style.opacity = "0";
         document.getElementById("linesButton").style.transform = "rotate(-90deg)";
         document.getElementById("linesButton").style.transition = "all 0.5s ease-in-out";
         document.getElementById("main-page-contents").style = "-webkit-filter: blur(2px); filter: blur(2px); margin-top: 40px;";
@@ -26,9 +20,6 @@ function openMenu() {
     } else {
         document.getElementById('menu-toggle').checked = true;
         menuOpened = false;
-        document.getElementById("menucloser").style.opacity = "0";
-        document.getElementById("menucloser").style.transform = "rotate(90deg)";
-        document.getElementById("linesButton").style.opacity = "1";
         document.getElementById("linesButton").style.transform = "rotate(0deg)";
         document.getElementById("linesButton").style.transition = "all 0.5s ease-in-out";
         document.getElementById("main-page-contents").style = "-webkit-filter: blur(0px); filter: blur(0px); margin-top: 40px;";
